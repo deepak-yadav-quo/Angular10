5d8cae1d11a7bb45fc6e1e4a2b690bbbbd48627a
Deepak Yadav <deepak.yadav@quovantis.com>
2020-08-05 18:18:19 +0530

    RoutingConcepts Implementation

--- food-recipe-shopping-app/src/app/recipes/recipe.service.ts
+++ food-recipe-shopping-app/src/app/recipes/recipe.service.ts
@@ -28,10 +28,14 @@ export class RecipeService {
     constructor(private slService: ShoppingListService) {
 
     }
+
     getRecipes() {
         return this.recipes.slice();
     }
 
+    getRecipe(index: number) {
+        return this.recipes[index];
+    }
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.slService.addIngredients(ingredients);
     }
